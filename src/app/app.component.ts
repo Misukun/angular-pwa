ac98a4d18a5766c2b91ce3961752095f793cb4fe
David Vaquero <pepesan@gmail.com>
2018-10-04 12:07:53 +0200

    Comprobaciones de PWA respecto a  versiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private swu: SwUpdate) {
+    // Comprobación de versiones disponibles
+    swu.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+    });
+    // Actualiza la app cuando esté disponible
+    swu.available.subscribe(event => {
+      if (this.muestraUpdate(event)) {
+        swu.activateUpdate().then(() => document.location.reload());
+      }
+    });
+    // Verifica la versión activada
+    swu.activated.subscribe(event => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
+    // Forzar la Comprobación de versiones
+    interval(6 * 60 * 60).subscribe(() => swu.checkForUpdate());
+  }
+  muestraUpdate(evento) {
+    // Debería ser visual con una alerta
+    console.log(evento);
+  }
 }
